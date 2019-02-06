13536e773c3295015368de7f467677e7c0f20f3a
hiro0218 <hiro0218@gmail.com>
2019-02-06 21:04:55 +0900

    init: packeage.json / prettier format

--- wp-netlify-updater.php
+++ wp-netlify-updater.php
@@ -7,61 +7,75 @@ Author: yahsan2
 Version: 0.1
 Author URI: https://github.com/yahsan2
 */
-class WpNetlifyUpdater {
-    function __construct() {
-      $this->version = '0.1';
-      $this->name = 'Wp Netlify Updater';
-      $this->slug = 'wp-netlify-updater';
-      $this->prefix = 'wpnu_';
+class WpNetlifyUpdater
+{
+    function __construct()
+    {
+        $this->version = '0.1';
+        $this->name = 'Wp Netlify Updater';
+        $this->slug = 'wp-netlify-updater';
+        $this->prefix = 'wpnu_';
 
-      $this->set_options();
+        $this->set_options();
 
-      if(is_admin()){
-        add_action('save_post', array($this, 'netlify_webhooks'));
-        add_action('admin_menu', array($this, 'add_menu'));
-      }
+        if (is_admin()) {
+            add_action('save_post', array($this, 'netlify_webhooks'));
+            add_action('admin_menu', array($this, 'add_menu'));
+        }
     }
 
-    function netlify_webhooks() {
-      if(isset($this->webhook_url) && $this->webhook_url){
-        $url = $this->webhook_url;
-        $data = array();
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-      }
+    function netlify_webhooks()
+    {
+        if (isset($this->webhook_url) && $this->webhook_url) {
+            $url = $this->webhook_url;
+            $data = array();
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            curl_close($curl);
+        }
     }
 
-    function add_menu() {
-      add_submenu_page('options-general.php', $this->name, $this->name,  'level_8', __FILE__, array($this,'option_page'), '');
+    function add_menu()
+    {
+        add_submenu_page(
+            'options-general.php',
+            $this->name,
+            $this->name,
+            'level_8',
+            __FILE__,
+            array($this, 'option_page'),
+            ''
+        );
     }
 
-    function set_options(){
-      $opt = get_option( $this->prefix.'option');
-      if(isset($opt)){
-        $this->webhook_url = $opt['webhook_url'];
-      }else{
-        $this->webhook_url = null;
-      }
+    function set_options()
+    {
+        $opt = get_option($this->prefix . 'option');
+        if (isset($opt)) {
+            $this->webhook_url = $opt['webhook_url'];
+        } else {
+            $this->webhook_url = null;
+        }
     }
 
-    function option_page() {
-      if ( isset($_POST[ $this->prefix.'option'])): ?>
+    function option_page()
+    {
+        if (isset($_POST[$this->prefix . 'option'])): ?>
       <?php
-          check_admin_referer( $this->slug );
-          $opt = $_POST[ $this->prefix.'option'];
-          update_option( $this->prefix.'option', $opt);
+      check_admin_referer($this->slug);
+      $opt = $_POST[$this->prefix . 'option'];
+      update_option($this->prefix . 'option', $opt);
       ?>
       <div class="updated fade"><p><strong><?php _e('Options saved.'); ?></strong></p></div>
       <?php endif; ?>
       <div class="wrap">
-        <h2><?php echo $this->name ?></h2>
+        <h2><?php echo $this->name; ?></h2>
         <form action="" method="post">
             <?php
-            wp_nonce_field( $this->slug );
+            wp_nonce_field($this->slug);
             $this->set_options();
             ?>
             <table class="form-table">
@@ -72,7 +86,7 @@ class WpNetlifyUpdater {
                     </td>
                     <td>
                       <p>URL:</p>
-                      <input name="<?php echo $this->prefix; ?>option[webhook_url]" type="text" id="input_url" value="<?php  echo $this->webhook_url ?>" class="regular-text" placeholder="https://api.netlify.com/build_hooks/xxxxxxxxxxxxxxxxxxxxxxxx" />
+                      <input name="<?php echo $this->prefix; ?>option[webhook_url]" type="text" id="input_url" value="<?php echo $this->webhook_url; ?>" class="regular-text" placeholder="https://api.netlify.com/build_hooks/xxxxxxxxxxxxxxxxxxxxxxxx" />
                     </td>
                 </tr>
             </table>
